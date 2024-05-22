f7538dbb25abf029196d3cb3960d42f741146528
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-05-23 00:51:40 +0300

    fix removeAllContacts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,10 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
-// видалення контактів
+
 export const removeAllContacts = async () => {
+  let contacts = [];
   try {
-    await fs.writeFile(PATH_DB, [], 'utf8');
-    console.log('All contacts have been removed.');
+    // читаемо і записуємо дані до масиву
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    contacts = JSON.parse(data);
+    // перевіряємо наявність контактів
+    if (contacts.length > 0) {
+      // видаляємо контакти перезаписуючи порожнім масивом
+      await fs.writeFile(PATH_DB, '[]', 'utf8');
+      console.log('All contacts have been removed.');
+    } else {
+      console.log('No contacts to remove.');
+    }
   } catch (err) {
     console.error('Error', err);
   }
